0dfc7e2d1ae37386a95c67d1be370e441e8bb33e
공대표 <c5ngsdp@gmail.com>
2024-06-11 23:24:17 +0900

    Section 7. 자바 메모리 구조와 static
    static 변수 3

--- src/static1/DataCountMain3.java
+++ src/static1/DataCountMain3.java
@@ -8,5 +8,9 @@ public class DataCountMain3 {
 
         Data3 data2 = new Data3("B");
         System.out.println("B count = " + Data3.count); // 클래스에 직접 접근
+
+        Data3 data4 = new Data3("D");
+        System.out.println(data4.count); // 인스턴스를 통한 접근
+        System.out.println(Data3.count); // 클래스를 통한 접근
     }
 }
