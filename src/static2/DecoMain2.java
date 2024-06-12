4bb7e9e739985877883bed8935af0127f31dae9e
공대표 <c5ngsdp@gmail.com>
2024-06-12 19:58:48 +0900

    Section 7. 자바 메모리 구조와 static
    static 메서드 1

---
+++ src/static2/DecoMain2.java
@@ -0,0 +1,12 @@
+package static2;
+
+public class DecoMain2 {
+    public static void main(String[] args) {
+        String s = "hello java";
+
+        String deco = DecoUtil2.deco(s); // 정적 메서드 호출
+
+        System.out.println("before: " + s);
+        System.out.println("after: " + deco);
+    }
+}
