af4f3b849f561d64f2852ad44482252d60f3aa0c
공대표 <c5ngsdp@gmail.com>
2024-06-24 21:23:24 +0900

    Section 5. 열거형 - ENUM
    타입 안전 열거형 패턴

---
+++ Inflearn_Java_Mid_01/src/enumeration/ex2/ClassGradeEx2_2.java
@@ -0,0 +1,12 @@
+package enumeration.ex2;
+
+public class ClassGradeEx2_2 {
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+//        ClassGrade classGrade = new ClassGrade(); // 생성자를 private으로 막아서 생성 못하게 한다.
+//        int result = discountService.discount(classGrade, price);
+//        System.out.println("result = " + result);
+    }
+}
