07ccf5e8da0e5dd3deecc190f341e32b13e9f5c9
공대표 <c5ngsdp@gmail.com>
2024-06-24 20:59:58 +0900

    Section 5. 열거형 - ENUM
    문자열과 타입 안전성1

---
+++ Inflearn_Java_Mid_01/src/enumeration/StringGradeEx0_2.java
@@ -0,0 +1,21 @@
+package enumeration;
+
+public class StringGradeEx0_2 {
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+
+        // 개발자가 실수로 존재하지 않는 등급을 입력
+        int vip = discountService.discount("VIP", price);
+        System.out.println("vip = " + vip);
+
+        int basic = discountService.discount("BASIC", price);
+        int gold = discountService.discount("GOLD", price);
+        int diamond = discountService.discount("DIAMOND", price);
+
+        System.out.println("basic = " + basic);
+        System.out.println("gold = " + gold);
+        System.out.println("diamond = " + diamond);
+    }
+}
