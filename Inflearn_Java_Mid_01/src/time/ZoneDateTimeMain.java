3f6d54579af627fcac2f27770274de718f83e253
공대표 <c5ngsdp@gmail.com>
2024-06-25 20:26:30 +0900

    Section 6. 날짜와 시간
    타임존 - ZonedDateTime

---
+++ Inflearn_Java_Mid_01/src/time/ZoneDateTimeMain.java
@@ -0,0 +1,23 @@
+package time;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class ZoneDateTimeMain {
+    public static void main(String[] args) {
+        ZonedDateTime nowZdt = ZonedDateTime.now();
+        System.out.println("nowZdt = " + nowZdt);
+
+        LocalDateTime ldt = LocalDateTime.of(2030, 1, 1, 13, 30, 50);
+        // ZonedDateTime 은 zone만 추가된 형태
+        ZonedDateTime zdt1 = ZonedDateTime.of(ldt, ZoneId.of("Asia/Seoul"));
+        System.out.println("zdt1 = " + zdt1);
+
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(2030, 1, 1, 13, 30, 50, 0, ZoneId.of("Asia/Seoul"));
+        System.out.println("zonedDateTime = " + zonedDateTime);
+
+        ZonedDateTime utc = zonedDateTime.withZoneSameInstant(ZoneId.of("UTC"));
+        System.out.println("utc = " + utc);
+    }
+}
