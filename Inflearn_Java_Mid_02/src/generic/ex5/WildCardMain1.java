8e10c3e5da5f9cea500c3b0edc8d76bfcee1beb5
공대표 <c5ngsdp@gmail.com>
2024-07-02 22:15:53 +0900

    Section 2. 제네릭 - Generic2
    와일드 카드 2

--- Inflearn_Java_Mid_02/src/generic/ex5/WildCardMain1.java
+++ Inflearn_Java_Mid_02/src/generic/ex5/WildCardMain1.java
@@ -19,7 +19,11 @@ public class WildCardMain1 {
         WildCardEx.printWildCardV2(dogBox);
 
         Dog dog = WildCardEx.printAndReturnGeneric(dogBox);
-        Animal animal = WildCardEx.printAndReturnWildCard(dogBox);
+
+        catBox.set(new Cat("냥냥이", 200));
+        Cat cat = WildCardEx.printAndReturnGeneric(catBox);
+
+        Animal animal = WildCardEx.printAndReturnWildCard(dogBox); // 와일드카드를 사용하면 무조건 상한으로 리턴된다. 즉 여기선 Animal로 return 된다.
 
     }
 }
