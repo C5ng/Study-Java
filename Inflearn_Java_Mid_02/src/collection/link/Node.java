cacd5da86e1393c014e4a044cf15719f0122a3e5
공대표 <c5ngsdp@gmail.com>
2024-07-03 21:40:21 +0900

    Section 4. 컬렉션 프레임워크 - LinkedList
    노드와 연결 2

--- Inflearn_Java_Mid_02/src/collection/link/Node.java
+++ Inflearn_Java_Mid_02/src/collection/link/Node.java
@@ -7,4 +7,30 @@ public class Node {
     public Node(Object item) {
         this.item = item;
     }
+
+//    @Override
+//    public String toString() {
+//        return "Node{" +
+//                "item=" + item +
+//                ", next=" + next +
+//                '}';
+//    } // next는 Node 타입이다. 즉 toString()을 또 호출한다.
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node x = this;
+        sb.append("[");
+        while (x != null) {
+            sb.append(x.item);
+            if (x.next != null) {
+                sb.append("->");
+            }
+            x = x.next;
+        }
+        sb.append("]");
+
+        return sb.toString();
+    }
 }
