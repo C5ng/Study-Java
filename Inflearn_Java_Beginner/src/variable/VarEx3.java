52c246f9aaf0c657d70e1b40f7561b2931c33a74
공대표 <c5ngsdp@gmail.com>
2024-07-09 22:10:20 +0900

    Section 2. 변수
    문제와 풀이

---
+++ Inflearn_Java_Beginner/src/variable/VarEx3.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long num = 10_000_000_000L;
+        boolean flag = true;
+
+        System.out.println(num);
+        System.out.println(flag);
+    }
+}
