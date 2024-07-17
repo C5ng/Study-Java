154b11cbdee73fef7d1c899259df97e89235bab1
공대표 <c5ngsdp@gmail.com>
2024-07-17 21:25:08 +0900

    Section 9. 컬렉션 프레임워크 - Map, Stack, Queue
    컬렉션 프레임워크 - Map 구현체

---
+++ Inflearn_Java_Mid_02/src/collection/map/JavaMapMain.java
@@ -0,0 +1,26 @@
+package collection.map;
+
+import java.util.*;
+
+public class JavaMapMain {
+    public static void main(String[] args) {
+        run(new HashMap<>());
+        run(new LinkedHashMap<>());
+        run(new TreeMap<>());
+    }
+
+    private static void run(Map<String, Integer> map) {
+        System.out.println("map = " + map.getClass());
+        map.put("C", 10);
+        map.put("B", 20);
+        map.put("A", 30);
+        map.put("1", 40);
+        map.put("2", 50);
+
+        Set<String> keySet = map.keySet();
+        for (String s : keySet) {
+            System.out.println("key: " + s + ", value: " + map.get(s));
+        }
+        System.out.println();
+    }
+}
